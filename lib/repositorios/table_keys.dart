a68a62cef1130b603c0bdddaef10ded3e8663d80
LucasTheisen31 <lucasevandro11@hotmail.com>
2022-06-25 16:03:15 -0300

    Commit 5 25/06/22

---
+++ lib/repositorios/table_keys.dart
@@ -0,0 +1,7 @@
+
+/*Padronização das chaves (nome das colunas das tabelas para evitar de digitar errado) do banco de dados*/
+
+const keyUserName = 'name';
+const keyUserEmail = 'email';
+const keyUserPhone = 'phone';
+const keyUserPassword = 'password';
