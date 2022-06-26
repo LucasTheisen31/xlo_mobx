6b5a642d144894d05855f4b9a35d47e6a63df5c1
LucasTheisen31 <lucasevandro11@hotmail.com>
2022-06-25 22:08:23 -0300

    Commit 8 25/06/22

--- lib/repositorios/user_repositorio.dart
+++ lib/repositorios/user_repositorio.dart
@@ -2,9 +2,8 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/models/user.dart';
 import 'package:xlo_mobx/repositorios/table_keys.dart';
 
-class UserRepositorio{
-
-  cadastrar(User user){
+class UserRepositorio {
+  cadastrar(User user) {
     final parseUser = ParseUser(user.email, user.passwrod, user.email);
 
     parseUser.set<String>(keyUserName, user.name);
@@ -12,5 +11,7 @@ class UserRepositorio{
     parseUser.set<String>(keyUserPhone, user.phone);
     /*type.index (0 = PARTICULAR, 1 = PROFESSIONAL)*/
     parseUser.set(keyUserType, user.type.index);
+
+    parseUser.signUp(); //registra o usuario no banco
   }
 }
